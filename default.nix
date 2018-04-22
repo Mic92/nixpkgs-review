d5533dc2d26b896e763cd39dea338d5e98cc0632
Jörg Thalheim <joerg@thalheim.io>
2018-04-22 12:39:55 +0100

    run linter/typechecks in travis/nix

--- default.nix
+++ default.nix
@@ -1,9 +1,14 @@
 with import <nixpkgs> {};
 
-python3Packages.buildPythonApplication {
+python3.pkgs.buildPythonApplication {
   name = "nix-review";
   src = ./.;
   buildInputs = [ makeWrapper ];
+  checkInputs = [ mypy ];
+  checkPhase = ''
+    ${python3.interpreter} -m unittest discover .
+    mypy nix_review
+  '';
   preFixup = ''
     wrapProgram $out/bin/nix-review --prefix PATH : ${nix}/bin
   '';
