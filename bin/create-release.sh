d095c75a0906213ca8a4a0e6771cc8adfb4a8f65
Jörg Thalheim <joerg@thalheim.io>
2018-08-30 07:09:34 +0100

    add create release script

---
+++ bin/create-release.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -eu -o pipefail
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+cd $SCRIPT_DIR/..
+
+version=${1:-}
+if [[ -z "$version" ]]; then
+  echo "USUAGE: $0 version" 2>/dev/null
+  exit 1
+fi
+
+if [[ "$(git symbolic-ref --short HEAD)" != "master" ]]; then
+  echo "must be on master branch" 2>/dev/null
+  exit 1
+fi
+
+sed -i -e "s!version=\".*\"!version=\"${version}\"!" setup.py
+git add setup.py
+git commit -m "bump version ${version}"
+git tag "${version}"
+
+echo 'now run `git push --tags origin master`'
