f0d823c6601b01e28bd00a9e3ecc363d4966e828
Jörg Thalheim <joerg@thalheim.io>
2018-08-30 07:31:02 +0100

    fix overriding NIXPKGS_CONFIG
    the temporary file was deleted to early

--- nix_review/app.py
+++ nix_review/app.py
@@ -169,25 +169,32 @@ class Worktree:
             )
             raise
         self.worktree_dir: Optional[str] = worktree_dir
-        with tempfile.NamedTemporaryFile() as cfg:
-            cfg.write(b"pkgs: { allowUnfree = true; }")
-            cfg.flush()
-            self.environ = os.environ.copy()
-            os.environ["NIXPKGS_CONFIG"] = cfg.name
-            os.environ["NIX_PATH"] = f"nixpkgs={os.path.realpath(worktree_dir)}"
+        self.nixpkgs_config = tempfile.NamedTemporaryFile()
+        self.nixpkgs_config.write(b"pkgs: { allowUnfree = true; }")
+        self.nixpkgs_config.flush()
+
+        self.environ = os.environ.copy()
+        os.environ["NIXPKGS_CONFIG"] = self.nixpkgs_config.name
+        os.environ["NIX_PATH"] = f"nixpkgs={os.path.realpath(worktree_dir)}"
 
     def __enter__(self) -> str:
         assert self.worktree_dir is not None
         return self.worktree_dir
 
     def __exit__(self, exc_type: Any, exc_value: Any, traceback: Any) -> None:
+        if self.nixpkgs_config is not None:
+            self.nixpkgs_config.close()
+
+        if self.environ is not None:
+            os.environ.update(self.environ)
+
         if self.worktree_dir is None:
             return
+
         with DisableKeyboardInterrupt():
             shutil.rmtree(self.worktree_dir)
             sh(["git", "worktree", "prune"])
             os.environ.clear()
-            os.environ.update(self.environ)
 
 
 def main(command: str, raw_args: List[str]) -> None:
