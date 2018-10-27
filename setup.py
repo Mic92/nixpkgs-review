352fc47bbe81721b914cfc3aa2b8b61ca4362ab6
Jörg Thalheim <joerg@thalheim.io>
2018-10-27 17:53:09 +0100

    bump version 0.6.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ assert sys.version_info >= (3, 6, 0), "nix-review requires Python 3.6+"
 
 setup(
     name="nix-review",
-    version="0.5.3",
+    version="0.6.0",
     description="Review nixpkgs pull requests",
     author="Jörg Thalheim",
     author_email="joerg@thalheim.io",
