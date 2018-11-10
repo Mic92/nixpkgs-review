700b5f97f03b50573f2b6a9db2d51749fe2a414d
Jörg Thalheim <joerg@thalheim.io>
2018-11-10 00:20:37 +0000

    default.nix: add flake8

--- default.nix
+++ default.nix
@@ -1,20 +1,24 @@
 { pkgs ?  import <nixpkgs> {} }:
 
-pkgs.python3.pkgs.buildPythonApplication rec {
+
+with pkgs;
+python3.pkgs.buildPythonApplication rec {
   name = "nix-review";
   src = ./.;
-  env = pkgs.buildEnv { inherit name; paths = buildInputs ++ checkInputs; };
-  buildInputs = [ pkgs.makeWrapper ];
-  checkInputs = [ pkgs.mypy pkgs.python3.pkgs.black pkgs.glibcLocales ];
+  env = buildEnv { inherit name; paths = buildInputs ++ checkInputs; };
+  buildInputs = [ makeWrapper ];
+  checkInputs = [ mypy python3.pkgs.black python3.pkgs.flake8 glibcLocales ];
   checkPhase = ''
     echo -e "\x1b[32m## run unittest\x1b[0m"
     ${pkgs.python3.interpreter} -m unittest discover .
-    echo -e "\x1b[32m## run mypy\x1b[0m"
-    mypy nix_review
     echo -e "\x1b[32m## run black\x1b[0m"
     LC_ALL=en_US.utf-8 black --check .
+    echo -e "\x1b[32m## run flake8\x1b[0m"
+    flake8 nix_review
+    echo -e "\x1b[32m## run mypy\x1b[0m"
+    mypy nix_review
   '';
   makeWrapperArgs = [
-    "--prefix PATH" ":" "${pkgs.nix}/bin"
+    "--prefix PATH" ":" "${nix}/bin"
   ];
 }
