449ad8808a6280518c6b26a20ca0402f737b7dc1
Jörg Thalheim <joerg@thalheim.io>
2020-02-18 10:39:01 +0000

    fix evaluation if python package is marked as broken
    fixes #82

--- nixpkgs_review/nix/evalAttrs.nix
+++ nixpkgs_review/nix/evalAttrs.nix
@@ -7,11 +7,11 @@ let
 
   attrs = fromJSON (readFile attr-json);
   getProperties = name: let
-    path = lib.splitString "." name;
-    pkg = lib.attrByPath path null pkgs;
+    attrPath = lib.splitString "." name;
+    pkg = lib.attrByPath attrPath null pkgs;
     maybePath = builtins.tryEval "${pkg}";
   in rec {
-    exists = pkg != null;
+    exists = lib.hasAttrByPath attrPath pkgs;
     broken = !exists || !maybePath.success;
     path = if !broken then maybePath.value else null;
     drvPath = if !broken then pkg.drvPath else null;
