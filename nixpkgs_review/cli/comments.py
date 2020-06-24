638830f20900c207eaaca5f9220b2b506dcb2a11
Jörg Thalheim <joerg@thalheim.io>
2020-06-24 06:38:16 +0100

    comments: move printing logic to cli module

--- nixpkgs_review/cli/comments.py
+++ nixpkgs_review/cli/comments.py
@@ -6,4 +6,14 @@ from .utils import ensure_github_token, get_current_pr
 
 def show_comments(args: argparse.Namespace) -> None:
     github_client = GithubClient(ensure_github_token(args.token))
-    github_client.comments(get_current_pr())
+    comments = github_client.comments(get_current_pr())
+
+    for comment in comments:
+        if "diff_hunk" in comment:
+            diff_lines = comment["diff_hunk"].split("\n")
+            start_line = max(0, comment["original_line"] - 2)
+            end_line = comment["original_line"] + 1
+            for line in diff_lines[start_line:end_line]:
+                print(line)
+
+        print(f"{comment['user']['login']}: {comment['body']}\n")
--- nixpkgs_review/github.py
+++ nixpkgs_review/github.py
@@ -2,7 +2,7 @@ import json
 import urllib.parse
 import urllib.request
 from collections import defaultdict
-from typing import Any, DefaultDict, Dict, Optional, Set
+from typing import Any, DefaultDict, Dict, Optional, Set, List
 
 
 def pr_url(pr: int) -> str:
@@ -57,23 +57,13 @@ class GithubClient:
         print(f"Merging  {pr_url(pr)}")
         return self.put(f"/repos/NixOS/nixpkgs/pulls/{pr}/merge")
 
-    def comments(self, pr: int) -> Any:
+    def comments(self, pr: int) -> List[Dict[str, Any]]:
         "Show comments of a PR"
         issue_comments = self.get(f"repos/NixOS/nixpkgs/issues/{pr}/comments")
         pulls_comments = self.get(f"repos/NixOS/nixpkgs/pulls/{pr}/comments")
-        sorted_comments = sorted(
+        return sorted(
             issue_comments + pulls_comments, key=lambda x: x["created_at"]
         )
-
-        for comment in sorted_comments:
-            if "diff_hunk" in comment:
-                diff_lines = comment["diff_hunk"].split("\n")
-                start_line = max(0, comment["original_line"] - 2)
-                end_line = comment["original_line"] + 1
-                for line in diff_lines[start_line:end_line]:
-                    print(line)
-            print(f"{comment['user']['login']}: {comment['body']}")
-            print("")
 
     def pull_request(self, number: int) -> Any:
         "Get a pull request"
