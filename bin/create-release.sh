1ec51238313252a9483f5c13526e2a1c69dfb52a
Jörg Thalheim <joerg@thalheim.io>
2020-11-26 08:06:56 +0100

    release script: check if are up-to-date

--- bin/create-release.sh
+++ bin/create-release.sh
@@ -7,15 +7,27 @@ cd $SCRIPT_DIR/..
 
 version=${1:-}
 if [[ -z "$version" ]]; then
-  echo "USAGE: $0 version" 2>/dev/null
+  echo "USAGE: $0 version" >&2
   exit 1
 fi
 
 if [[ "$(git symbolic-ref --short HEAD)" != "master" ]]; then
-  echo "must be on master branch" 2>/dev/null
+  echo "must be on master branch" >&2
   exit 1
 fi
 
+# ensure we are up-to-date
+uncommited_changes=$(git diff --compact-summary)
+if [[ -n "$uncommited_changes" ]]; then
+  echo -e "There are uncommited changes, exiting:\n${uncommited_changes}" >&2
+  exit 1
+fi
+git pull git@github.com:Mic92/nixpkgs-review master
+unpushed_commits=$(git log --format=oneline origin/master..master)
+if [[ "$unpushed_commits" != "" ]]; then
+  echo -e "\nThere are unpushed changes, exiting:\n$unpushed_commits" >&2
+  exit 1
+fi
 sed -i -e "s!version=\".*\"!version=\"${version}\"!" setup.py
 git add setup.py
 nix-build default.nix
