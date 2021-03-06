60205b9a291a4af7d66f796a4922f0aa3744d27a
Jörg Thalheim <joerg@thalheim.io>
2021-03-06 10:19:26 +0100

    fix mypy check

--- default.nix
+++ default.nix
@@ -26,7 +26,7 @@ python3.pkgs.buildPythonApplication rec {
     echo -e "\x1b[32m## run flake8\x1b[0m"
     flake8 .
     echo -e "\x1b[32m## run mypy\x1b[0m"
-    mypy --strict .
+    mypy --strict nixpkgs_review
   '';
   makeWrapperArgs = [
     "--prefix PATH : ${lib.makeBinPath [ nixFlakes git ]}"
