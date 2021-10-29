97361fc37566faa296391e78f778fa0d45ca7a60
Jörg Thalheim <joerg@thalheim.io>
2021-10-29 21:13:42 +0200

    update flakes

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   description = "nixpkgs-review";
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system: {
