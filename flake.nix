9001b946cf4700ab9936b24ea3d0a1a0d9c1f32c
Thiago Kenji Okada <thiagokokada@gmail.com>
2021-12-14 13:46:52 -0300

    Only expose nixpkgs-review-sandbox on Linux systems

--- flake.nix
+++ flake.nix
@@ -5,17 +5,20 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system: {
-      packages = {
-        nixpkgs-review = import ./. {
+    nixpkgs.lib.foldr nixpkgs.lib.recursiveUpdate { } [
+      (flake-utils.lib.eachDefaultSystem (system: {
+        packages.nixpkgs-review = import ./. {
           pkgs = nixpkgs.legacyPackages.${system};
         };
-        nixpkgs-review-sandbox = import ./. {
+
+        defaultPackage = self.packages.${system}.nixpkgs-review;
+      }))
+
+      (flake-utils.lib.eachSystem [ "aarch64-linux" "i686-linux" "x86_64-linux" ] (system: {
+        packages.nixpkgs-review-sandbox = import ./. {
           pkgs = nixpkgs.legacyPackages.${system};
           withSandboxSupport = true;
         };
-      };
-
-      defaultPackage = self.packages.${system}.nixpkgs-review;
-    });
+      }))
+    ];
 }
