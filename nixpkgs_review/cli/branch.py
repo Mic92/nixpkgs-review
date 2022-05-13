09abf9c2ed3ddca339c95a53e77bc5d16cba35ae
Milan Hauth <milahu@gmail.com>
2022-05-13 13:23:43 +0200

    move class Branch to util.py

--- nixpkgs_review/cli/branch.py
+++ nixpkgs_review/cli/branch.py
@@ -11,7 +11,7 @@ from urllib.parse import urlparse, unquote
 from ..builddir import Builddir
 from ..buildenv import Buildenv
 from ..review import CheckoutOption, Review
-from ..utils import warn
+from ..utils import warn, Branch
 from .utils import ensure_github_token
 
 # TODO maybe use https://github.com/nephila/giturlparse
@@ -29,86 +29,6 @@ from .utils import ensure_github_token
 # bdb177925a580798ba152774be1f66e88472912e        refs/heads/milahu-patch-1
 # ec031ad1f3e405893ead8954dfab16aecd07f809        refs/heads/a/b/c/d/test
 # ec031ad1f3e405893ead8954dfab16aecd07f809        refs/heads/github.com/a/b/c/d
-
-class Branch:
-
-    remote = "https://github.com/NixOS/nixpkgs"
-    #base = "master" # TODO implement? allow comparing to other branches (staging ...)
-    branch = None
-    commit = None
-
-    def __repr__(self):
-        return f"Branch:\n  remote: {self.remote}\n  branch: {self.branch}\n  commit: {self.commit}"
-
-    def __init__(self, raw_input):
-
-        if not ":" in raw_input:
-            # compare a nixpkgs branch to the nixpkgs master branch
-            self.branch = raw_input
-            # input is not a url, so it's not urlencoded
-            #self.branch = unquote(raw_input)
-            return
-
-        url = urlparse(raw_input)
-
-        if not url.scheme in {"https", "git", "git+ssh"}: # TODO more?
-            # format is user:branch
-            # example: alice:a/b-c+d&e%f
-            parts = raw_input.split(":")
-            if len(parts) != 2:
-                raise Exception(f"branch expected format user:branch, got {repr(raw_input)}")
-            [owner, branch] = parts
-            self.remote = f"https://github.com/{owner}/nixpkgs"
-            self.branch = branch
-            # input is not a url, so it's not urlencoded
-            # copy branch from github webinterface https://github.com/milahu/random/pull/5
-            # -> a/b-c+d&e%f(g)hijk=l.m]__@n#op<q>r§t"u'v`w´x;y_z
-            #self.branch = unquote(branch)
-            return
-
-        if url.netloc != "github.com":
-            # TODO implement: gitlab, gitea, ... maybe use https://github.com/nephila/giturlparse
-            raise Exception(f"not implemented. branch must be a github url, got {repr(raw_input)}")
-
-        dirs = url.path.split("/")
-
-        if dirs[2] != "nixpkgs":
-            raise Exception(f"branch repo name must be nixpkgs, got {repr(raw_input)}")
-
-        if dirs[3] == "tree":
-            self.branch = unquote("/".join(dirs[4:]))
-            return
-
-        if dirs[3] == "commit":
-            self.commit = dirs[4]
-            owner = dirs[1]
-            self.remote = f"https://github.com/{owner}/nixpkgs"
-            return
-
-        if dirs[3] == "compare":
-            if not raw_input.startswith("https://github.com/NixOS/nixpkgs/compare/master.."):
-                raise Exception(f"expected github compare link versus nixpkgs master, got {repr(raw_input)}")
-
-            branch = unquote("/".join(dirs[4:]))
-            if branch.startswith("master..."):
-                branch = branch[9:]
-            elif branch.startswith("master.."):
-                branch = branch[8:]
-
-            parts = branch.split(":")
-            if len(parts) == 2:
-                [owner, branch] = parts
-                self.remote = f"https://github.com/{owner}/nixpkgs"
-                self.branch = branch
-                return
-            if len(parts) == 1:
-                # remote is NixOS/nixpkgs
-                self.branch = branch
-                return
-            raise Exception(f"expected github compare link with user:branch or branch, got {repr(raw_input)}")
-
-        raise Exception(f"failed to parse branch from {repr(raw_input)}")
-
 
 def parse_branches(branch_args: List[str]) -> List[int]:
     branches: List[int] = []
