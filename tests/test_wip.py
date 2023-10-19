67345db9d1c7c0963c5a37f3be404d051e958714
Artturin <Artturin@artturin.com>
2023-10-19 09:16:44 +0300

    mypy check everything
    move the strict enablement to the pyproject.toml so disabling checks
    works correctly because `--strict` takes precedence over the file
    
    Checking everything was disabled in 60205b9a due to (seemingly) a bug

--- tests/test_wip.py
+++ tests/test_wip.py
@@ -10,7 +10,7 @@ from .conftest import Helpers
 
 
 @pytest.mark.skipif(not shutil.which("nom"), reason="`nom` not found in PATH")
-def test_wip_command(helpers: Helpers, capfd) -> None:
+def test_wip_command(helpers: Helpers, capfd: pytest.CaptureFixture) -> None:
     with helpers.nixpkgs() as nixpkgs:
         with open(nixpkgs.path.joinpath("pkg1.txt"), "w") as f:
             f.write("foo")
@@ -24,7 +24,9 @@ def test_wip_command(helpers: Helpers, capfd) -> None:
         assert "$ nom build" in captured.out
 
 
-def test_wip_command_without_nom(helpers: Helpers, capfd) -> None:
+def test_wip_command_without_nom(
+    helpers: Helpers, capfd: pytest.CaptureFixture
+) -> None:
     with helpers.nixpkgs() as nixpkgs:
         with open(nixpkgs.path.joinpath("pkg1.txt"), "w") as f:
             f.write("foo")
