1c06ac032cbae5d178887ac3323c66b3ac0b10de
Jörg Thalheim <joerg@thalheim.io>
2024-10-09 08:14:45 +0200

    modernize formatters

--- treefmt.nix
+++ treefmt.nix
@@ -1,53 +1,32 @@
-{ lib, inputs, ... }: {
+{ lib, inputs, ... }:
+{
   imports = [
     inputs.treefmt-nix.flakeModule
   ];
 
-  perSystem = { pkgs, ... }: {
-    treefmt = {
-      # Used to find the project root
-      projectRootFile = "flake.lock";
+  perSystem =
+    { pkgs, ... }:
+    {
+      treefmt = {
+        # Used to find the project root
+        projectRootFile = "flake.lock";
 
-      programs.deno.enable = true;
-      programs.mypy.enable = true;
-      programs.mypy.directories = {
-        "." = {
-          directory = ".";
-          extraPythonPackages = [
-            pkgs.python3.pkgs.pytest
-          ];
-        };
-      };
+        programs.deno.enable =
+          pkgs.lib.meta.availableOn pkgs.stdenv.hostPlatform pkgs.deno && !pkgs.deno.meta.broken;
+        programs.ruff.format = true;
+        programs.ruff.check = true;
+        programs.mypy.enable = true;
+        programs.nixfmt.enable = pkgs.lib.meta.availableOn pkgs.stdenv.buildPlatform pkgs.nixfmt-rfc-style.compiler;
+        programs.deadnix.enable = true;
 
-      settings.formatter = {
-        nix = {
-          command = "sh";
-          options = [
-            "-eucx"
-            ''
-              # First deadnix
-              ${lib.getExe pkgs.deadnix} --edit "$@"
-              # Then nixpkgs-fmt
-              ${lib.getExe pkgs.nixpkgs-fmt} "$@"
-            ''
-            "--"
-          ];
-          includes = [ "*.nix" ];
-        };
-
-        python = {
-          command = "sh";
-          options = [
-            "-eucx"
-            ''
-              ${lib.getExe pkgs.ruff} --fix "$@"
-              ${lib.getExe pkgs.ruff} format "$@"
-            ''
-            "--" # this argument is ignored by bash
-          ];
-          includes = [ "*.py" ];
+        programs.mypy.directories = {
+          "." = {
+            directory = ".";
+            extraPythonPackages = [
+              pkgs.python3.pkgs.pytest
+            ];
+          };
         };
       };
     };
-  };
 }
