dd26f701f8cfbcfbbd6f48e8bbc1b325aa2ce232
Jörg Thalheim <joerg@thalheim.io>
2025-07-07 12:00:20 +0200

    configure author/committer for all git commands

--- nixpkgs_review/cli/wip.py
+++ nixpkgs_review/cli/wip.py
@@ -1,17 +1,17 @@
 import argparse
 from pathlib import Path
 
+from nixpkgs_review import git
 from nixpkgs_review.allow import AllowedFeatures
 from nixpkgs_review.buildenv import Buildenv
 from nixpkgs_review.review import review_local_revision
-from nixpkgs_review.utils import verify_commit_hash
 
 
 def wip_command(args: argparse.Namespace) -> Path:
     allow = AllowedFeatures(args.allow)
     with Buildenv(allow.aliases, args.extra_nixpkgs_config) as nixpkgs_config:
         return review_local_revision(
-            f"rev-{verify_commit_hash('HEAD')}-dirty",
+            f"rev-{git.verify_commit_hash('HEAD')}-dirty",
             args,
             allow,
             nixpkgs_config,
