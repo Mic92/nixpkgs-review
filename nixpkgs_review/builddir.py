2bde0ea57b05e3b3042212fb2f6a7c4fa1d01fef
Jörg Thalheim <joerg@thalheim.io>
2025-09-21 20:34:04 +0200

    Move NIX_PATH mutation into context manager
    Set NIX_PATH environment variable only within the context manager's
    lifetime to prevent side effects during object construction.

--- nixpkgs_review/builddir.py
+++ nixpkgs_review/builddir.py
@@ -74,15 +74,15 @@ class Builddir:
 
         self.worktree_dir = self.path / "nixpkgs"
         self.worktree_dir.mkdir()
-        nix_path = [
+        self._nix_path_parts = [
             f"nixpkgs={self.worktree_dir}",
             f"nixpkgs-overlays={self.overlay.path}",
         ]
-        # we don't actually use this, but its handy for users who want to try out things with the current nixpkgs version.
-        os.environ["NIX_PATH"] = ":".join(nix_path)
-        self.nix_path = " ".join(nix_path)
+        self.nix_path = " ".join(self._nix_path_parts)
 
     def __enter__(self) -> Self:
+        # we don't actually use this, but its handy for users who want to try out things with the current nixpkgs version.
+        os.environ["NIX_PATH"] = ":".join(self._nix_path_parts)
         return self
 
     def __exit__(
