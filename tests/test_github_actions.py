752c28245aa590ccd21108773945dea816ab7a23
Antoine du Hamel <duhamelantoine1995@gmail.com>
2025-11-17 19:47:15 +0100

    fixup! check for labels

--- tests/test_github_actions.py
+++ tests/test_github_actions.py
@@ -29,7 +29,10 @@ def test_merge(mock_urlopen: MagicMock, helpers: Helpers) -> None:
     with helpers.save_environ():
         os.environ["PR"] = "1"
         os.environ["GITHUB_TOKEN"] = "foo"  # noqa: S105
-        mock_urlopen.side_effect = [mock_open(read_data="{}")()]
+        mock_urlopen.side_effect = [
+            mock_open(read_data="[]")(),
+            mock_open(read_data="{}")()
+        ]
         main("nixpkgs-review", ["merge"])
 
 
